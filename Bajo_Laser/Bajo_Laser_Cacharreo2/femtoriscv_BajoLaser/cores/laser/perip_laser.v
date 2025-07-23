de9e28ccca53280cefcc2d1136c3b55da6ad23ce
JuanF14 <jgaitann@unal.edu.co>
2025-07-23 18:10:41 -0500

    Update perip_laser.v

--- Bajo_Laser/Bajo_Laser_Cacharreo2/femtoriscv_BajoLaser/cores/laser/perip_laser.v
+++ Bajo_Laser/Bajo_Laser_Cacharreo2/femtoriscv_BajoLaser/cores/laser/perip_laser.v
@@ -1,4 +1,4 @@
-module perip_laser (
+module perip_laser (  //
     input clk,
     input rst,
     input [31:0] d_in, // Valor de entrada
@@ -7,7 +7,7 @@ module perip_laser (
     input rd,
     input wr,
     output [31:0] d_out, // Valor de salida
-    input wire [3:0] laser
+    input wire [3:0] laser //Vector de 4 bits que representa el estado digital de los sensores láser
 );
     localparam integer GETLASER = 5'h02; // C #define GETLASER 0x02
     //Leer los laser
