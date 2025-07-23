ca42863e81c16207e754d875e9f22c2b12795aee
JuanF14 <jgaitann@unal.edu.co>
2025-07-23 17:22:38 -0500

    a
    a

--- Bajo_Laser/Bajo_Laser_Cacharreo2/femtoriscv_BajoLaser/firmware/c-code/laser.c
+++ Bajo_Laser/Bajo_Laser_Cacharreo2/femtoriscv_BajoLaser/firmware/c-code/laser.c
@@ -13,24 +13,24 @@ volatile uint32_t *const get_laser = (uint32_t *)(LASER + GET_LASER);
 volatile uint32_t *const gp = (uint32_t *)IO_BASE;
 
 // Buffers de mensaje
-char osc_msg[32];  // Mensaje completo OSC
-char laser_str[8]; // Valor leído del láser convertido a string
+char osc_msg[32];  // Cadena de texto que se enviará por UART en formato OSC (Open Sound Control).
+char laser_str[8]; // Valor leído del láser convertido a string, en si, contendrá el número leído del láser convertido a cadena.
 
 int main() {
-  const char topic[] = "osc /dev1/piano ";  // Tópico OSC
+  const char topic[] = "osc /dev1/piano ";  // Esta es la cadena base del mensaje OSC, que indica el dispositivo y el canal que emite el dato.
 
   putstring("start LASER\n\r");
 
   while (1) {
     // Leer el valor digital del sensor láser
-    uint8_t laserin = *get_laser;
+    uint8_t laserin = *get_laser; //Lee el valor desde el registro mapeado al sensor láser.
 
     // Convertir ese valor a string
-    itoa_simple_signed(laserin, laser_str);
+    itoa_simple_signed(laserin, laser_str); //Convierte el número (0 o 1, por ejemplo) a una cadena de caracteres para poder imprimirlo o enviarlo como texto.
 
     // Construir el mensaje OSC en el buffer
     osc_msg[0] = '\0';               // Vaciar buffer
-    mi_strcat(osc_msg, topic);       // Agregar el topic
+    mi_strcat(osc_msg, topic);       // Agregar el arreglo ritmico
     mi_strcat(osc_msg, "i ");        // Formato integer
     mi_strcat(osc_msg, laser_str);   // Agregar valor del láser
     mi_strcat(osc_msg, "\r\n");      // Final del mensaje
